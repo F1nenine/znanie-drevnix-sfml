71a9fb664fada929e1bbbbaf5bca9c81a5178e54
Bombinos <Egorsergeev8@yandex.ru>
2025-10-27 20:39:50 +0300

    Прототип будущего header

---
+++ header/Святопись_крест_крест.h
@@ -0,0 +1,61 @@
+#ifndef Святопись
+#define Святопись
+
+#include "Ве_крест_крест.h"
+
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/Network.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
+
+#define сп sf
+
+// Оконное творение
+#define ОкноСозерцанья RenderWindow
+#define ОконныйВид VideoMode
+#define ОконныйВариант WindowStyle
+#define Окно Window
+#define Плоть Context
+
+// Зрительные образы  
+#define Образопись Texture
+#define Ликодвижный Sprite
+#define Письмовица Font
+#define Письмопись Text
+#define ОбразоСложение Shape
+#define Круголикий CircleShape
+#define Четыреугольник RectangleShape
+#define Многоликий ConvexShape
+
+// Цветословие
+#define Цвет Color
+#define ЦветоСмешение BlendMode
+#define ЦветоПреставление ColorTransform
+
+// Звукословие
+#define Звукословие Sound
+#define Звуковидица SoundBuffer
+#define Музыкословие Music
+#define Слухословие AudioDevice
+#define Слухопространство SoundSpace
+
+// Временословие  
+#define Часомерие Clock
+#define Время Time
+#define Миговость Instant
+
+// Ввододействие
+#define Событие Event
+#define Клавишеведение Keyboard
+#define Мышеведение Mouse
+#define Джойстиковедение Joystick
+#define Сенсороведение Sensor
+
+// Пространстводействие
+#define Напровление Vector2
+#define Прямоугол Rect
+#define Преображень Transform
+#define Видосвод View
+
+#endif
